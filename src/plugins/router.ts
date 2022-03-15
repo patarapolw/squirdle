93fe9b98016675b37230ccb15e78655f2e29f3a4
Pacharapol Withayasakpunt <polv@polv.cc>
2022-03-15 14:33:19 +0700

    remove /:lang routes

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,45 +1,27 @@
 import {
   NavigationGuardWithThis,
-  RouteLocationNormalized,
   createRouter,
   createWebHistory
 } from 'vue-router'
 
-import { dailyJSON, lang as lang0, linkTemplate, translation } from '../assets'
+import { dailyJSON, lang, linkTemplate, translation } from '../assets'
 
 const translationImport = import.meta.glob('../../translation/*.json')
 
 const beforeGameEnter: NavigationGuardWithThis<undefined> = (to, _, next) => {
-  let lang = rGetLang(to)!
-
-  if (lang && lang.length !== 2) {
-    next({
-      path: '/404'
-    })
-    return
-  }
-
-  if (!lang) {
-    lang = lang0.value
-  }
-
-  const makeImport = () => `../../translation/${lang}.json`
-  if (!makeImport()) {
-    lang = window.LANG
-  }
+  const makeImport = () => `../../translation/${lang.value}.json`
 
   const doImport = translationImport[makeImport()]
   const linkTemplateImport =
-    translationImport[`../../translation/${lang}-ref.json`] ||
+    translationImport[`../../translation/${lang.value}-ref.json`] ||
     translationImport[`../../translation/en-ref.json`]
   // @ts-ignore
   if (doImport) {
     Promise.all([doImport(), linkTemplateImport()]).then(([t, ln]) => {
-      lang0.value = lang
       translation.value = t.default
       linkTemplate.value = ln.default
 
-      if (lang === 'ja') {
+      if (lang.value === 'ja') {
         import('wanakana').then((r) => (window.wanakana = r))
       }
 
@@ -49,37 +31,19 @@ const beforeGameEnter: NavigationGuardWithThis<undefined> = (to, _, next) => {
   }
 
   next({
-    path: '/404',
-    query: { lang }
+    path: '/404'
   })
 }
 
 const dailyImport = import.meta.glob('../../generated/*/daily.json')
 
 const getDaily: NavigationGuardWithThis<undefined> = (to, _, next) => {
-  let lang = rGetLang(to)!
-
-  if (lang && lang.length !== 2) {
-    next({
-      path: '/404'
-    })
-    return
-  }
-
-  if (!lang) {
-    lang = lang0.value
-  }
-
-  const makeImport = () => `../../generated/${lang}/daily.json`
-  if (!makeImport()) {
-    lang = window.LANG
-  }
+  const makeImport = () => `../../generated/${lang.value}/daily.json`
 
   const doImport = dailyImport[makeImport()]
   // @ts-ignore
   if (doImport) {
     doImport().then((t) => {
-      lang0.value = lang
       dailyJSON.value = t.default
       next()
     })
@@ -88,12 +52,8 @@ const getDaily: NavigationGuardWithThis<undefined> = (to, _, next) => {
 
   next({
     path: '/404',
-    query: { lang, daily: 'true' }
+    query: { daily: 'true' }
   })
-}
-
-function rGetLang(r: RouteLocationNormalized) {
-  return (r.params.lang || r.query.lang)?.toString?.()
 }
 
 export const router = createRouter({
@@ -103,31 +63,18 @@ export const router = createRouter({
       path: '/daily',
       alias: ['/'],
       component: () => import('../pages/Game.vue'),
-      props: (r) => ({ daily: true, lang: rGetLang(r) }),
+      props: (r) => ({ daily: true }),
       beforeEnter: [beforeGameEnter, getDaily]
     },
     {
       path: '/free',
       component: () => import('../pages/Game.vue'),
-      props: (r) => ({ daily: false, lang: rGetLang(r) }),
+      props: (r) => ({ daily: false }),
       beforeEnter: beforeGameEnter
     },
     {
       path: '/404',
       component: () => import('../pages/404.vue')
-    },
-    {
-      path: '/:lang/daily',
-      alias: ['/:lang'],
-      component: () => import('../pages/Game.vue'),
-      props: (r) => ({ daily: true, lang: rGetLang(r) }),
-      beforeEnter: [beforeGameEnter, getDaily]
-    },
-    {
-      path: '/:lang/free',
-      component: () => import('../pages/Game.vue'),
-      props: (r) => ({ daily: false, lang: rGetLang(r) }),
-      beforeEnter: beforeGameEnter
     },
     {
       path: '/:pathMatch(.*)*',
